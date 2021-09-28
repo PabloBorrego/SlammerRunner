4498125672d4763aa9ef008c20d81b0441db2bbf
PabloBorrego <pabloborregovidal@gmail.com>
2021-09-28 11:17:57 +0200

    Primera version movimiento guardias

--- SlammerRunner0.0/Assets/Scripts/Movimiento/Guardia_Movement.cs
+++ SlammerRunner0.0/Assets/Scripts/Movimiento/Guardia_Movement.cs
@@ -4,36 +4,37 @@ using UnityEngine;
 
 public class Guardia_Movement : MonoBehaviour
 {
-
+    
+    //V Publicas
     public float velocidadMov = 3f;
     public float velocidadRot = 5f;
-
-
+    public int AnguloVistazo = 80;
     public Transform[] puntosPatrulla;
     public float tiempoEsperaInicio;
     public Rigidbody2D rb;
 
+    //V Privadas
     private int puntoDestino;
     private float tiempoEspera;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         puntoDestino = 0;
         tiempoEspera = tiempoEsperaInicio;
     }
-
-    // Update is called once per frame
     void Update()
     {
+        //Rota y se desplaza
         RotarGuardia(puntoDestino);
         MoverGuardia();
     }
 
+
+
+    //Implementación del movimiento(Predefinido)
     public void MoverGuardia()
     {
-        
         //Se mueve hacia el siguiente punto
         transform.position = Vector2.MoveTowards(
                transform.position, puntosPatrulla[puntoDestino].position, velocidadMov * Time.deltaTime);
@@ -41,35 +42,36 @@ public class Guardia_Movement : MonoBehaviour
         //Checkea si ha llegado al punto, espera el tiempo asignado, y elige un nuevo punto
         if (Vector2.Distance(transform.position, puntosPatrulla[puntoDestino].position) < 0.2f)
         {
+
+            //Si el tiempo ha llegado a cero
             if (tiempoEspera <= 0)
             {
-                
-
                 puntoDestino += 1;
                 tiempoEspera = tiempoEsperaInicio;
 
+                // Si has llegado al final de los puntos de destino reset de ruta
                 if (puntoDestino >= puntosPatrulla.Length)
                 {
-                    Debug.LogError("Resetea");
                     puntoDestino = 0;
-                    Debug.Log(puntoDestino);
                 }
             }
+
+            //Si el tiempo no ha llegado a cero, rota sobre si mismo hasta que acaba el tiempo
             else
             {
                 tiempoEspera -= Time.deltaTime;
+                transform.Rotate(0, 0, AnguloVistazo * Time.deltaTime);
             }
 
         }
     }
 
 
+    //Calculo de la tangente del angulo para rotar en la direccion al siguiente punto de destino
     public void RotarGuardia(int i)
     {
-
         Vector2 direccion = puntosPatrulla[i].position - transform.position;
         float angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
         rb.rotation = angulo;
-
     }
 }
