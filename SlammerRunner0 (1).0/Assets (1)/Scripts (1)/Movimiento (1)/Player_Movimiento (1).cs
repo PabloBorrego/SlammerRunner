ce6d72308c3913b51f39ad20ec32d5b92ca7fd0f
PabloBorrego <pabloborregovidal@gmail.com>
2021-09-28 09:43:26 +0200

    Añadir proyecto

---
+++ SlammerRunner0 (1).0/Assets (1)/Scripts (1)/Movimiento (1)/Player_Movimiento (1).cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movimiento : MonoBehaviour
+{
+
+    public float velocidadMov = 5f;
+    public Rigidbody2D rb;
+    public Vector2 movimientoDir;
+
+
+    //Espacio para los inputs del movimiento
+    void Update()
+    {
+        //Inputs
+        movimientoDir.x = Input.GetAxisRaw("Horizontal");
+        movimientoDir.y = Input.GetAxisRaw("Vertical"); 
+    }
+
+
+    //Espacio para ejecutar el movimiento
+    private void FixedUpdate()
+    {
+           //Movimiento con velocidad constante
+           rb.MovePosition(rb.position + movimientoDir * velocidadMov * Time.fixedDeltaTime);
+    }
+
+}
