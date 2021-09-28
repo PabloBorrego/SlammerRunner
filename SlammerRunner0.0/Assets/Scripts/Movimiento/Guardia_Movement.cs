7626dc39b4920effca12b3f45e6beed4f5e49c52
PabloBorrego <pabloborregovidal@gmail.com>
2021-09-28 10:00:13 +0200

    Importacion del proyecto segundo intento

---
+++ SlammerRunner0.0/Assets/Scripts/Movimiento/Guardia_Movement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guardia_Movement : MonoBehaviour
+{
+
+    public float velocidadMov = 3f;
+    public float velocidadRot = 5f;
+
+
+    public Transform[] puntosPatrulla;
+    public float tiempoEsperaInicio;
+    
+    private int puntoDestino;
+    private float tiempoEspera;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puntoDestino = 0;
+        tiempoEspera = tiempoEsperaInicio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverGuardia();
+    }
+
+    public void MoverGuardia() {
+        //Se mueve hacia el siguiente punto
+        transform.position = Vector2.MoveTowards(
+               transform.position, puntosPatrulla[puntoDestino].position, velocidadMov * Time.deltaTime);
+
+        //Checkea si ha llegado al punto, espera el tiempo asignado, y elige un nuevo punto
+        if (Vector2.Distance(transform.position, puntosPatrulla[puntoDestino].position) < 0.2f)
+        {
+            if (tiempoEspera <= 0)
+            {
+                    puntoDestino += 1;
+                    Debug.Log(puntoDestino);
+                  //  RotarGuardia(puntoDestino);
+                    tiempoEspera = tiempoEsperaInicio;
+
+                if (puntoDestino >= puntosPatrulla.Length) {
+                    Debug.LogError("Resetea");
+                    puntoDestino = 0;
+                    Debug.Log(puntoDestino);
+                }
+            }
+            else
+            {
+                tiempoEspera -= Time.deltaTime;
+            }
+
+        }
+    }
+
+    /**
+    public void RotarGuardia(int i) {
+
+        Vector2 direccion = puntosPatrulla[i].position - transform.position;
+        float angulo = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
+        Quaternion rotacion = Quaternion.AngleAxis(angulo, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotacion, velocidadRot * Time.deltaTime);
+    }
+    */
+}
