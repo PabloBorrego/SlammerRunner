3b4c3c2fd67617cbfb615d58eabd57624ff8b409
PabloBorrego <pabloborregovidal@gmail.com>
2021-09-28 13:02:25 +0200

    Update Player_Movimiento.cs
    Añadida rotación del personaje cuando camina.

--- SlammerRunner0.0/Assets/Scripts/Movimiento/Player_Movimiento.cs
+++ SlammerRunner0.0/Assets/Scripts/Movimiento/Player_Movimiento.cs
@@ -8,6 +8,7 @@ public class Player_Movimiento : MonoBehaviour
     public float velocidadMov = 5f;
     public Rigidbody2D rb;
     public Vector2 movimientoDir;
+    public float velocidadRot = 5f;
 
 
     //Espacio para los inputs del movimiento
@@ -22,8 +23,15 @@ public class Player_Movimiento : MonoBehaviour
     //Espacio para ejecutar el movimiento
     private void FixedUpdate()
     {
-           //Movimiento con velocidad constante
-           rb.MovePosition(rb.position + movimientoDir * velocidadMov * Time.fixedDeltaTime);
+        //Movimiento con velocidad constante
+        rb.MovePosition(rb.position + movimientoDir * velocidadMov * Time.fixedDeltaTime);
+
+        //Si se está moviendo cambia lar toación hacia el frente
+        if (movimientoDir != Vector2.zero) {
+
+            Quaternion dirRotacion = Quaternion.LookRotation(Vector3.forward, movimientoDir);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, dirRotacion, velocidadRot * Time.deltaTime); 
+        }
     }
 
 }
