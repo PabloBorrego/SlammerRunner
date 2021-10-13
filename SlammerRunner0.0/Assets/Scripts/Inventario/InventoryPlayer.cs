f933a6c21b22e9f589c04906e730974aa0b6e76b
PabloBorrego <pabloborregovidal@gmail.com>
2021-10-13 21:07:38 +0200

    Inventario
    No aparece el icono dentro de la ranura

---
+++ SlammerRunner0.0/Assets/Scripts/Inventario/InventoryPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryPlayer : MonoBehaviour
+{
+    public bool[] estaLleno;
+    public GameObject[]objetos;
+
+}
--- SlammerRunner0.0/Assets/Scripts/Inventario/Pickup.cs
+++ SlammerRunner0.0/Assets/Scripts/Inventario/Pickup.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    private InventoryPlayer inventario;
+    public GameObject itemImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Se carga la instancia par poder manipular el inventario de 
+        inventario = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryPlayer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player")) {
+            for (int i = 0; i<inventario.objetos.Length; i++) {
+                if (inventario.estaLleno[i] == false) {
+                    inventario.estaLleno[i] = true;
+                    inventario.objetos[i] = gameObject;
+                    Instantiate(itemImage, inventario.objetos[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+
+
+
     }
 }
