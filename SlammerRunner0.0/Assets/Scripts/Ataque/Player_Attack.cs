14ebb292a07c0fff30fd3ff6ff0f70c4f1c27039
PabloBorrego <pabloborregovidal@gmail.com>
2021-11-10 21:28:56 +0100

    Inclusión de sprites prototipo en ataque y pruebas con tilemap

---
+++ SlammerRunner0.0/Assets/Scripts/Ataque/Player_Attack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Attack : MonoBehaviour
+{
+
+    public GameObject arma;
+    private bool atacando;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arma.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            atacando = true;
+        }
+        else {
+            atacando = false;
+        }
+
+        while (atacando.Equals(true)) {
+            arma.SetActive(true);
+        }
+        
+    }
+}
